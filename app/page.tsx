4f1e8cc3952e874154b2ba501a1c93c68e145caf
Abdullah Azhari <aaazhari_4@yahoo.com>
2026-03-28 18:21:42 +0300

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,15 @@ export default function Home() {
     <div className="flex flex-col items-center justify-center h-screen bg-black">
       <img src="/lamphome.png" alt="LAMP" className="w-1/2" />
 
-      <button className="mt-2 px-6 py-2 border border-white text-white rounded-lg hover:bg-white hover:text-black transition">
-        Contact Us
-      </button>
+      <a
+  href="https://wa.me/966500032297"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  <button className="mt-2 px-6 py-2 border border-white text-white rounded-lg hover:bg-white hover:text-black transition">
+    Contact Us
+  </button>
+</a>
     </div>
   );
 }
