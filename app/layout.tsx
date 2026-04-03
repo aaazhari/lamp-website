288c42398e385dc3667fc8a679a5c9cdd748a8fd
Abdullah Azhari <aaazhari_4@yahoo.com>
2026-04-03 16:22:11 +0300

    add pacman game

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Script from "next/script";
+import { GoogleTagManager } from "@next/third-parties/google";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -45,31 +45,9 @@ export default function RootLayout({
 
 <body className="min-h-full flex flex-col">
 
-{/* ✅ Google Tag Manager */}
-<Script
-  id="gtm-script"
-  strategy="afterInteractive"
->
-  {`
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-MS5943PN');
-  `}
-</Script>
+  <GoogleTagManager gtmId="GTM-MS5943PN" />
 
-{/* ✅ NoScript fallback */}
-<noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-MS5943PN"
-    height="0"
-    width="0"
-    style={{ display: "none", visibility: "hidden" }}
-  />
-</noscript>
-
-{children}
+  {children}
 
 </body>
     </html>
