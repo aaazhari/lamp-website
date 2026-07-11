ba109b168855999b00bd44f108de245558f55372
Abdullah Azhari <aaazhari_4@yahoo.com>
2026-07-11 18:18:58 +0300

    add whatsapp coffee cover for invitation

--- app/games/coffee/page.tsx
+++ app/games/coffee/page.tsx
@@ -1,16 +1,16 @@
 import CoffeeClient from "./CoffeeClient";
 
 export const metadata = {
-  title: "Bring Your Kid To Work Day Invitation",
-  description: "Join us for a special experience with LAMP Event",
+  title: "AbrarDose OverCamp Invitation",
+  description: "Join us for a special Summer & Fun Vibes experience with OverDose",
   openGraph: {
-    title: "Bring Your Kid To Work Day Invitation",
-    description: "Join us for a special experience with LAMP Event",
+    title: "AbrarDose OverCamp Invitation",
+    description: "Join us for a special Summer & Fun Vibes experience with OverDose",
     url: "https://lampevent.com/games/coffee",
     siteName: "LAMP Event",
     images: [
       {
-        url: "https://lampevent.com/ihc-invitation.png",
+        url: "https://lampevent.com/overcamp-invitation.png",
         width: 1200,
         height: 630,
       },
